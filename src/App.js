c40a349842792e31a01cf1d601ffe085da99442b
bishal00sharma <bishalsharmaece@gmail.com>
2022-12-15 21:33:50 +0530

    redux signIn signUp completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import { AllRoutes } from "./AllRoutes/AllRoutes";
-import SignIn from "../src/Pages/Sign/SignIn"
 
 function App() {
   return (
     <div className="App">
-      {/* <AllRoutes /> */}
-      <SignIn />
+      <AllRoutes />
     </div>
   );
 }
