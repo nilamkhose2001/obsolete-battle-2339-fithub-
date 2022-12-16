7c4f708807816d0b71b6996cf8d5988be27f5dfc
nilamkhose2001 <nilamkhose2001@gmail.com>
2022-12-16 14:33:46 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { AllRoutes } from "./AllRoutes/AllRoutes";
+import { Navbar } from "./Components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <AllRoutes />
     </div>
   );
