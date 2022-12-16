f0af8bfc3b75f71b72241fb2b5d5631cd7f0be2a
amandk5 <adkofficial01@gmail.com>
2022-12-16 15:08:38 +0530

    day-4 work

--- src/Components/Programs/CardComponent.jsx
+++ src/Components/Programs/CardComponent.jsx
@@ -1,33 +1,57 @@
-import { Box, Button, Flex, Image, Text, useMediaQuery } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Image,
+  Text,
+  useMediaQuery,
+} from "@chakra-ui/react";
 import { BiShoppingBag } from "react-icons/bi";
 
-export default function CardComponent() {
+// const initState = {
+//     _id: "",
+//     contentshref: "",
+//     exclusiveitem: "",
+//     image: "",
+//     week: "",
+//     time: "",
+//     desc: "",
+//     price: "",
+//     category: "",
+//   };
+export default function CardComponent({
+  contentshref,
+  exclusiveitem,
+  image,
+  week,
+  time,
+  desc,
+  price,
+}) {
   const [isSmallerThan700] = useMediaQuery("(max-width: 380px)");
 
   return (
     <Box
-      w={!isSmallerThan700 ? "303px" : "100%"}
+      w={!isSmallerThan700 ? "300px" : "100%"}
       bg="white"
       border="3px solid #F0F4F6"
       _hover={{ border: "3px solid #4296CB" }}
     >
-      <Image
-        src="https://d18zdz9g6n5za7.cloudfront.net/plan/320/320-o_21_fb-meal-plan-eat-real-food-and-feel-great.jpg"
-        width="100%"
-        maxH="168px"
-      />
-      <Box margin="auto" w="90%" pt="5" pb="8">
-        <Text color="#4A9ACD" mb="1" fontSize="11px">
-          2 WEEK CHALLENGE - 23 MIN/DAY
-        </Text>
-        <Text fontWeight="bold" fontSize="15px">
-          Simply Stretch Challenge - Two Week Stretching Workout Plan
-        </Text>
-      </Box>
+      <a href={contentshref}>
+        <Image src={image} width="100%" maxH="168px" />
+        <Box margin="auto" w="90%" pt="5" pb="8">
+          <Text color="#4A9ACD" mb="1" fontSize="11px">
+            {week} - {time}
+          </Text>
+          <Text fontWeight="bold" fontSize="15px">
+            {desc}
+          </Text>
+        </Box>
+      </a>
       <Flex py="3" px="2" justifyContent="space-between">
         <Box>
           <Text>As Low As</Text>
-          <Text fontWeight="semibold">$7.99</Text>
+          <Text fontWeight="semibold">{price}</Text>
         </Box>
         <Box>
           <Button bg="#4296CB" color="white" height="100%">
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,15 +1,23 @@
-import {legacy_createStore,combineReducers,compose,applyMiddleware} from "redux"
+import {
+  legacy_createStore,
+  combineReducers,
+  compose,
+  applyMiddleware,
+} from "redux";
 import { authReducer } from "./auth/auth.reducer";
 
-import thunk from "redux-thunk"
+import thunk from "redux-thunk";
+import { programsReducer } from "./Programs/programs.reducer";
 //console.log(feedReducer)
-const rootReducer=combineReducers(
-    {
-        auth : authReducer,
-    }
-    )
+const rootReducer = combineReducers({
+  auth: authReducer,
+  programs: programsReducer,
+});
 
-    const createCompose=window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__||compose
+const createCompose = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export const store=legacy_createStore(rootReducer,createCompose(applyMiddleware(thunk)))
+export const store = legacy_createStore(
+  rootReducer,
+  createCompose(applyMiddleware(thunk))
+);
 //compose
