145b2415a7911bb91b298df396ce9ee52db20161
snikhil2001 <nikhilsahni2001@gmail.com>
2022-12-19 09:37:45 +0530

    minor fixes

--- src/Components/Home/NotSure.jsx
+++ src/Components/Home/NotSure.jsx
@@ -7,23 +7,19 @@ const NotSure = () => {
   const [data1, setData1] = useState([]);
 
   const getdata = async () => {
-    try{
+    try {
       let res = await axios.get(
         "https://fithub.onrender.com/products?category=program"
       );
-      setData1(res.data.product)
+      setData1(res.data.product);
       console.log(res.data);
-       
-    }catch(e){
+    } catch (e) {
       console.log(e);
     }
-   
-
   };
   useEffect(() => {
     getdata();
   }, []);
-
 
   return (
     <Box w="100%" h="auto" background={"#F0F4F6"} py={20}>
@@ -40,9 +36,17 @@ const NotSure = () => {
           gap={"5px"}
           columns={{ lg: 4, md: 3, sm: 2, base: 1 }}
         >
-           {data1?.slice(0, 4).map((el,i) => {
-            return <Card key={i} />;
-          })} 
+          {data1?.slice(0, 4).map((el, i) => {
+            return (
+              <Card
+                key={i}
+                image={el.image}
+                title={el.week}
+                subtitle={el.desc}
+                price={el.price}
+              />
+            );
+          })}
         </SimpleGrid>
       </Box>
     </Box>
